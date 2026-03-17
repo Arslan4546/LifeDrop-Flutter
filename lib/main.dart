abb23ef264ecb1925a40c3f261bfcdeb83046e59
Arslan4546 <arslantariq4546@gmail.com>
2026-03-17 13:29:19 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class LifeDropApp extends StatelessWidget {
         title: 'LifeDrop',
         theme: ThemeData(scaffoldBackgroundColor: AppColors.backgroundColor),
         onGenerateRoute: AppRoutes.generateRoute,
-        initialRoute: RouteNames.donorBottomNavView,
+        initialRoute: RouteNames.donorRequestView,
       ),
     );
   }
